38aa539bf14230f3c3f5d6efe1e00abb1bada78b
Soleon <soleonstudios@outlook.com>
2024-09-23 18:59:17 +1000

    chore: index file for all icons

---
+++ creative-factory-project/src/app/icons/index.ts
@@ -0,0 +1,10 @@
+import audSvg from './aud.svg';
+import cadSvg from './cad.svg';
+import gbpSvg from './gbp.svg';
+import nzdSvg from './nzd.svg';
+import usdSvg from './usd.svg';
+import euroSvg from './euro.svg';
+import calcSvg from './calcIcon.svg';
+import loadingSvg from './loading.svg';
+
+export { audSvg, cadSvg, calcSvg, euroSvg, gbpSvg, nzdSvg, usdSvg, loadingSvg };
